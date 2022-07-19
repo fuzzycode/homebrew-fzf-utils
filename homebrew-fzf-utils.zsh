ebb8a989e8d1d18f65904455366886a469fbcce4
Björn Larsson <b.e.larsson@gmail.com>
2022-07-19 20:52:15 +0200

    First iteration of functionality

--- homebrew-fzf-utils.zsh
+++ homebrew-fzf-utils.zsh
@@ -10,23 +10,60 @@ if ! (( $+commands[fzf] )); then
     exit
 fi
 
-function hfu_install() {
+HFU_INFO='brew info {}'
+HFU_BIND='ctrl-space:execute-silent(brew home {})'
 
+function hfu_install_formulae() {
+    local formulae=$(brew formulae | fzf --query="$1" -m --preview $HFU_INFO --bind $HFU_BIND)
+
+    if [[ $formulae ]]; then
+        for formula in $formulae; do
+            brew install $ƒormula
+        done
+    fi
+}
+
+function hfu_install_cask() {
+    local casks=$(brew casks | fzf --query="$1" -m --preview $HFU_INFO --bind $HFU_BIND)
+
+    if [[ $casks ]]; then
+        for cask in $casks; do
+            brew install $cask
+        done
+    fi
 }
 
 function hfu_uninstall() {
+    local leaves=$(brew leaves | fzf --query="$1" -m --preview $HFU_INFO --bind $HFU_BIND)
 
+    if [[ $leaves ]]; then
+        for leaf in $leaves; do
+            brew uninstall $leaf
+        done
+    fi
 }
 
 function hfu_update() {
+    local outdated=$(brew outdated -q | fzf --query="$1" -m --preview $HFU_INFO --bind $HFU_BIND)
 
+    if [[ $outdated ]]; then
+        for formula in $outdated; do
+            brew upgrade $ƒormula
+        done
+    fi
 }
 
 function hfu_init() {
-
+    alias bip=hfu_install_formulae
+    alias bic=hfu_install_cask
+    alias bup=hfu_update
+    alias brp=hfu_uninstall
 }
 
-autoload hfu_install
+autoload hfu_install_formulae
+autoload hfu_install_cask
 autoload hfu_uninstall
 autoload hfu_update
 autoload hfu_init
+
+hfu_init
